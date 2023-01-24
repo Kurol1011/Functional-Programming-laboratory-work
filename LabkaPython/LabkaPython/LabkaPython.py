bb5562b70ae8776b35209312af74f3deeabfd816
Olzhas Kurmangaliev <olzhaskurmangaliev335@gmail.com>
2023-01-24 15:49:59 +0600

    Lab-1

---
+++ LabkaPython/LabkaPython/LabkaPython.py
@@ -0,0 +1,32 @@
+name = "Olzhas"
+surname = "Kurmangaliev"
+#dataOfBirth = "09.03.2003"
+
+print("Имя: " + name)
+print("Фамилия: " + surname)
+
+backEndProgrammingLanguage = input("Введите ваш язык программирования:")
+age = int(input("Введите ваш возраст: "))
+level = "junior"
+
+
+
+
+if(age >= 18):
+    print("Вы можете работать полный рабочий день")
+else:
+    print("Вы можете работать не больше 3 часов в день")
+
+if(backEndProgrammingLanguage == "Java"):
+    bestFramework = "Spring"
+elif(backEndProgrammingLanguage == "Python"):
+    bestFramework = "Django"
+elif(backEndProgrammingLanguage == "PHP"):
+    bestFramework = "Laravel"
+elif(backEndProgrammingLanguage == "JavaScript"):
+    besbestFramework = "NodeJS"
+else:
+    bestFramework = ""
+
+print(("Лучший фреймворк для вашего языка для бэкенда это " + bestFramework) if len(bestFramework)>0 else "null")
+
