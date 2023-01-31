c2b74fc397c0049299ee846c55980515b9bbfe0f
Olzhas Kurmangaliev <olzhaskurmangaliev335@gmail.com>
2023-01-31 15:54:40 +0600

    lab-3

---
+++ LabkaPython-2/LabkaPython-2.py
@@ -0,0 +1,100 @@
+# import random
+
+#1
+# days = ["Monday" ,"Tuesday","Wednesday", "Thursday","Friday","Saturday","Sunday"]
+# k = 1
+# # for i in days:
+# #     print(f"{k} " + i)
+# #     k +=1
+# k = 7
+# print('\n')
+
+
+
+# while(k>0):
+#     print(f"{k} " + days[k-1])
+#     k-=1
+# print('\n')
+
+
+
+# print("range with 1 parametr:" + '\n')
+# for i in range(5):
+#     print(i)
+# print('\n')
+
+
+
+# print("range with 2 parametrs:" + '\n')
+# for i in range(5,10):
+#     print(i)
+# print('\n')
+
+
+
+# print("range with 3 parametrs:" + '\n')
+# for i in range(0,21,5):
+#     print(i)
+# print('\n')
+
+
+
+# print("range with randint[]:" + '\n')
+# for i in range(5):
+#     print(random.randint(2,10))
+# print('\n')
+
+
+
+# print("range with randint[):" + '\n')
+# for i in range(5):
+#     print(random.randrange(5,10))
+# print('\n')
+
+
+
+# print("range with randint:" + '\n')
+# for i in range(5):
+#     print(random.random() * (20 - 5) + 5)
+# print('\n')
+
+
+
+# a = int(input("Enter number a: "))
+# b = int(input("Enter number b: "))
+
+# for i in range(a,b+1):
+#     print(i)
+# print('\n')
+
+
+
+# a = int(input("Enter number a: "))
+# b = int(input("Enter number b: "))
+
+# if(a < b):
+#     for i in range(a,b+1):
+#         print(i)
+# else:
+#     for i in range(a, b-1 , -1):
+#         print(i)
+# print('\n')
+
+
+
+# a = int(input("Enter number a: "))
+# b = int(input("Enter number b: "))
+
+# for i in range(a,b+1,-1):
+#     print(i - (1 - (1 * (i%2))) )
+ 
+
+
+# n = int(input("Enter number n: "))
+# sum = 0
+# sumAllNum = 1
+# for i in range(1,n):
+#     a = int(input("Enter number: "))
+#     sum+=a
+#     sumAllNum+=i+1
+# print(sumAllNum - sum )
